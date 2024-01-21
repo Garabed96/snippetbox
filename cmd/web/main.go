81de8d4599994027c70e496b3246145c1abcb4f7
Garo <garonazarian09@gmail.com>
2024-01-21 13:49:50 +0700

    in-memory caching

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -12,9 +13,10 @@ import (
 )
 
 type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
-	snippets *models.SnippetModel
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
@@ -22,6 +24,7 @@ func main() {
 	dns := flag.String("dns", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
 	flag.Parse()
+
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)                  // Log to Stdout
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile) // Log to Stderr
 
@@ -30,27 +33,20 @@ func main() {
 		errorLog.Fatal(err)
 	}
 	defer db.Close()
+
+	// Initialize a new template cache...
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
 	// Initialize a new instance of app struct, containing dependencies
 	app := &application{
-		errorLog: errorLog,
-		infoLog:  infoLog,
-		snippets: &models.SnippetModel{DB: db},
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
-	// Use http.NewServeMux() function initialize a new multiplexer.
-	// Then, register func home as the handler/controller for the "/"
-	// Local scope ServerMux (SECURE) vs Global scope DefaultServeMux (INSECURE)
-
-	//INSECURE GLOBAL SCOPE
-	//http.HandleFunc("/", home)                    // Subtree path matching
-	//http.HandleFunc("/snippet/view", snippetView) // Fixed path matching
-	//http.HandleFunc("/snippet/create", snippetCreate)
-	//
-	//Use http.ListenAndServer to start a new web server (arg1, arg2):
-	//arg1 := TCP network address where server will listen for requests
-	//arg2 := servermux we created.
-	//
-	//log.Fatal() log the error message and exit the program.
-	//(Any error returned by http.ListenAndServe() is always non-nil)
 
 	srv := &http.Server{
 		Addr:     *addr,
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,43 @@
 package main
 
-import "snippetbox.garonazarian.net/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+	"snippetbox.garonazarian.net/internal/models"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// Creating in-memory map
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+
+		// Parse files into template set
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add template set to the map, using name of the page
+		cache[name] = ts
+	}
+	// return map
+	return cache, nil
 }
